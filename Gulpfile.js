b88ebd8e816844faa545b0a7509e7271beb8fe44
Mauro <mauro@insided.nl>
2015-10-28 21:51:18 +0100

    Clean up options

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,32 +1,33 @@
 var gulp = require('gulp'),
+    _ = require('underscore');
     sass = require('gulp-sass'),
     gulpif = require('gulp-if'),
     // Parameters needed for compile and watch
-    origin = './src/index.scss',
-    destination = './css',
-    watch = './src/**/*.scss',
-    browserSync = false,
-    taskSass = function(){
-      gulp.src(origin)
+    _options = {
+      origin: './src/index.scss',
+      destination: './css',
+      watch: './src/**/*.scss',
+      browserSync: false,
+      gulp: gulp
+    },
+    taskSass = function( options ){
+      var _pipe = options.gulp.src(options.origin)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(destination))
-        .pipe(gulpif(browserSync, browserSync.stream({match: '**/*.css'})));
+        .pipe(gulp.dest(options.destination));
+
+        if(options.browserSync) _pipe.pipe( gulpif(
+          options.browserSync, options.browserSync.stream({match: '**/*.css'})));
 
     },
-    taskWatch = function(){
-      gulp.watch(watch, ['sass']);
+    taskWatch = function( options ){
+      options.gulp.watch(options.watch, ['sass']);
     };
 
-gulp.task('sass', taskSass);
-gulp.task('watch:sass', taskWatch);
+gulp.task('sass', function(){ taskSass(_options); });
+gulp.task('watch:sass', function(){ taskWatch(_options); });
 
-module.exports = function(_origin, _destination, _watch, _gulp, _browserSync){
-  origin = _origin ? _origin : origin;
-  destination = _destination ? _destination : destination;
-  watch = _watch ? _watch : watch;
-  gulp = _gulp ? _gulp : gulp;
-  browserSync = _browserSync ? _browserSync : browserSync;
-
-  gulp.task('sass', taskSass);
-  gulp.task('watch:sass', taskWatch);
+module.exports = function(options){
+  options = _.extend( _options, options );
+  options.gulp.task('sass', function(){ taskSass(options); });
+  options.gulp.task('watch:sass', function(){ taskWatch(options); });
 };
