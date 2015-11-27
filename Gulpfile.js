5f0c9d1e4ddafc91201ee4a68ccc4d09a61081be
Mauro <mauro@insided.nl>
2015-11-27 20:52:34 +0100

    Clean up for a stable transformer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,16 @@
 var gulp = require('gulp'),
-    _ = require('underscore');
+    yargs = require('yargs'),
     sass = require('gulp-sass'),
-    gulpif = require('gulp-if'),
     // Parameters needed for compile and watch
-    _options = {
-      origin: './src/index.scss',
-      destination: './css',
-      watch: './src/**/*.scss',
-      browserSync: false,
-      gulp: gulp
-    },
-    taskSass = function( options ){
-      var _pipe = options.gulp.src(options.origin)
+    options = yargs.default({
+      origin: './src/bootstrap/index.scss',
+      destination: './css'
+    }).argv;
+
+gulp.task('sass', function(cb){
+    gulp.src(options.origin)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(options.destination));
+});
 
-        if(options.browserSync) _pipe.pipe( gulpif(
-          options.browserSync, options.browserSync.stream({match: '**/*.css'})));
-
-    },
-    taskWatch = function( options ){
-      options.gulp.watch(options.watch, ['sass']);
-    };
-
-gulp.task('sass', function(){ taskSass(_options); });
-gulp.task('watch:sass', function(){ taskWatch(_options); });
-
-module.exports = function(options){
-  options = _.extend( _options, options );
-  options.gulp.task('sass', function(){ taskSass(options); });
-  options.gulp.task('watch:sass', function(){ taskWatch(options); });
-};
+gulp.task('default', ['sass']);
